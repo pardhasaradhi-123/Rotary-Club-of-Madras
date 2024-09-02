6a85f3e3d0b69429e8894d28043482b0b200f27b
M Pardhasaradhi <150467752+pardhasaradhi-123@users.noreply.github.com>
2024-09-02 09:55:10 +0530

    Update AdminDashboard.jsx

--- frontend/src/Components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/Components/AdminDashboard/AdminDashboard.jsx
@@ -6,27 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function AdminDashboard() {
-  const [detailsReport, setDetailsReport] = useState([
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { id: 10 },
-    { id: 12 },
-    { id: 13 },
-    { id: 14 },
-    { id: 15 },
-    { id: 16 },
-    { id: 17 },
-    { id: 18 },
-    { id: 19 },
-    { id: 20 },
-  ]);
+  const [detailsReport, setDetailsReport] = useState([]);
   const [projectData, setProjectData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const todosPerPage = 5; // Number of items to display per page
